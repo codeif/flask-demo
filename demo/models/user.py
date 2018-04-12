b6f63aca3207489e174a0d53240071992a92ff2a
codeif <me@codeif.com>
2018-04-12 16:08:14 +0800

    password使用hybrid_property

--- demo/models/user.py
+++ demo/models/user.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask_login import UserMixin
 from sqlalchemy import DDL, event
+from sqlalchemy.ext.hybrid import hybrid_property
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from . import BaseModel
@@ -11,18 +12,19 @@ class User(BaseModel, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     phone = db.Column(db.String(20), unique=True)
     email = db.Column(db.String(191), unique=True)
-    password_hash = db.Column(db.String(128))
+    _password = db.Column('password', db.String(128))
 
-    @property
+    @hybrid_property
     def password(self):
+        # return self._password
         raise AttributeError('password is not a readable attribute')
 
     @password.setter
-    def password(self, password):
-        self.password_hash = generate_password_hash(password)
+    def password(self, value):
+        self._password = generate_password_hash(value)
 
-    def verify_password(self, password):
-        return check_password_hash(self.password_hash, password)
+    def check_password(self, password):
+        return check_password_hash(self._password, password)
 
 
 # UserID从1001开始
